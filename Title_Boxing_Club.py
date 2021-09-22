bed81325e9e0393b5fbfd8f1863f37aecfe3bc61
Mark J Ryan <90434341+MJR-24@users.noreply.github.com>
2021-09-22 15:54:11 -0400

    Add files via upload
    made lists for the table.
    added calculation for net salary.

--- Title_Boxing_Club.py
+++ Title_Boxing_Club.py
@@ -26,24 +26,28 @@
 
 # Print table
 def printTable():
-    d = {empName: ["lastName1+empID1", "annSal1", 'annSal1*intPer1',''],
-            empName: [22, 23.54, 'DOWN'],
-             empName: ["Ruby", 17.22, 'UP'],
-             empName: ["Lua", 10.55, 'DOWN'],
-             empName: ["Groovy", 9.22, 'DOWN'] }
-    print ("{:<10} {:<10} {:<10} {:<10}".format('Employee Code','Salary','Retirement','Net Salary'))
-    for k, v in d.items():
-        lang, perc, change = v
-        print ("{:<8} {:<15} {:<10} {:<10}".format(k, lang, perc, change))
+    headingList =["Employee Code\t", "Salary\t", "Retirement\t", "Net Salary"]
+    currprices = [555.55,333.33,111.11]
+    portfolioshares = [0,0,0]
+    print(*headingList)
+    for price,shares in zip(currprices,portfolioshares):
+        print(f"{price}\t{shares}")
+    for price,shares in zip(currprices,portfolioshares):
+        print(str(price).rjust(10) + str(shares).rjust(10))
 
 # Main
 i = 1
+codeList = []
+salList = []
+retList = []
+netList = []
+printTable()
 while i < 6:
     
 # Get employee name from user
     empName = input("Employee "+ str(i) + ": What is the employee's first and last name? ")
     lastName = empName.split()[1]    
-
+    
 # Get social security number from user
     SSNum = input("Employee "+ str(i) + ": What is the social security number? ")
     while len(SSNum) != 9:
@@ -51,6 +55,14 @@ while i < 6:
         SSNum = input("Employee "+ str(i) + ": What is the social security number? ")
     lastFour = SSNum[4:8]
     empCode = lastName+lastFour
+    codeList.append(empCode)
+
+# Get annual salary from user
+    annSal = input("Employee "+ str(i) + ": What is the annual salary? ")
+    while annSal.isdigit() == False:
+        print("Salary must be a number. Try again...")
+        annSal = input("Employee "+ str(i) + ": What is the annual salary? ")
+    salList.append(annSal)
     
 # Get retirement percentage from user
     retPer = input("Employee "+ str(i) + ": What percentage of the salary is contributed to retirement? ")
@@ -76,14 +88,18 @@ while i < 6:
             notInRange = 0
         elif (intPer >= 0) or (intPer <= 40):
             notInRange = 1
+    retList.append(retPer)
 
-# Get annual salary from user
-    annSal = input("Employee "+ str(i) + ": What is the annual salary? ")
-    while annSal.isdigit() == False:
-        print("Salary must be a number. Try again...")
-        annSal = input("Employee "+ str(i) + ": What is the annual salary? ")
-    
-    
+# Calculate net salary
+    netSalary = '{:.2f}'.format((intPer/100)*float(annSal))
+    print(netSalary)
+    netList.append(netSalary)
+
+    print(codeList)
+    print(salList)
+    print(retList)
+    print(netList)
+
     again = input("Is there another employee (y/n)? ")
 
     if (again == "y" or again == "Y" or again == "yes" or again == "Yes"):
@@ -91,4 +107,3 @@ while i < 6:
     elif (again == "n" or again == "N" or again == "no" or again == "No"):
         break
 
-printTable()
